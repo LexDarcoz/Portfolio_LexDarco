395ca8a437931c57e548a486e6bfa366ad345aa9
Alexander Schatteman <Alexander.schatteman@student.hogent.be>
2023-06-19 13:05:28 +0200

    V0.1 -> Finish project setup and initiated navbar

--- portfolio_scalexit/src/views/HomeView.vue
+++ portfolio_scalexit/src/views/HomeView.vue
@@ -1,15 +1,24 @@
 <template>
-  <hello-world />
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }"
+    ><NavbarHome />
+
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6">
+          <v-img src=""></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import NavbarHome from '../components/NavbarHome.vue';
+export default {
+  name: 'HomeLanding',
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+  components: {
+    NavbarHome,
+  },
+};
 </script>
