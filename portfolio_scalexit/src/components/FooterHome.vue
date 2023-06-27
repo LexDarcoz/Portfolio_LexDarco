937a08cfd98f20e39ac1ce3c8ac025c0f6ce6644
Alexander Schatteman <alexander.schatteman@student.hogent.be>
2023-06-27 17:35:06 +0200

    Add footer

---
+++ portfolio_scalexit/src/components/FooterHome.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer dark padless><v-card flat tile class="black white--text py-5 px-5 text-center"></v-card> </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
