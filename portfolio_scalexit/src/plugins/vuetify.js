009967355c4a422b27428bff61aa8173e1e7b903
Alexander Schatteman <Alexander.schatteman@student.hogent.be>
2023-07-01 12:59:47 +0200

    v0.2.2 Removed useless code

--- portfolio_scalexit/src/plugins/vuetify.js
+++ portfolio_scalexit/src/plugins/vuetify.js
@@ -1,16 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-import LexsitLogo from '@/components/LexsitLogo.vue';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
     iconfont: 'fa' || 'md' || 'mdi',
-    values: {
-      custom: {
-        component: LexsitLogo,
-      },
-    },
   },
   theme: {
     themes: {
